db2224ba307db08a44ad33b6e63c337bf4fdd231
Leslie1104091497 <saures1423@gmail.com>
2023-01-25 21:54:38 +0800

    navbar fixed

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -2,19 +2,27 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const NavbarComponent = () => {
 	const { pathname } = useLocation();
 	return (
 		<Navbar bg="light" expand="lg">
 			<Container>
-				<Navbar.Brand href="/">Pure Diamond Enterprise</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">
+					Pure Diamond Enterprise
+				</Navbar.Brand>
 
 				<Nav className="justify-content-center" activeKey={pathname}>
-					<Nav.Link href="/">Home</Nav.Link>
-					<Nav.Link href="/brand">Brand</Nav.Link>
-					<Nav.Link href="/item">Inventory Items</Nav.Link>
+					<Nav.Link as={Link} to="/">
+						Home
+					</Nav.Link>
+					<Nav.Link as={Link} to="/brand">
+						Brand
+					</Nav.Link>
+					<Nav.Link as={Link} to="/item">
+						Inventory Items
+					</Nav.Link>
 				</Nav>
 			</Container>
 		</Navbar>
--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ const queryClient = new QueryClient({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename="/">
 			<QueryClientProvider client={queryClient}>
 				<ToastContainer
 					position="top-right"
